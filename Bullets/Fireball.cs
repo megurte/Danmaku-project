1ddf908e3e9b6ccfde7f20ddc96c37f7c42bc1a4
megurt <mega.arte.99@gmail.com>
2022-06-12 15:26:36 +0300

    bulletType enum
    Signed-off-by: a.abramov <a.abramov@corp.nekki.ru>

--- Bullets/Fireball.cs
+++ Bullets/Fireball.cs
@@ -7,10 +7,16 @@ public class Fireball : MonoBehaviour
 {
     public float startSpeed = 0.1f;
     private bool _isActive = false;
-    public string bulletType = "Bullet";
+    public BulletType bulletType = BulletType.Fireball;
     public Vector3 direction;
     public float angle = 180;
-    public Vector3 rotation;
+    
+    public enum BulletType
+    {
+        Other,
+        Fireball,
+        Fire,
+    }
     
     private void FixedUpdate()
     {
@@ -22,7 +28,7 @@ public class Fireball : MonoBehaviour
         
         transform.Translate(direction.normalized * startSpeed, Space.World);
 
-        if (bulletType == "Fire")
+        if (bulletType == BulletType.Fire)
         {
             var degree = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(degree, Vector3.forward);
