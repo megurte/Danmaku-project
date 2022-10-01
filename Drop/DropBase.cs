e50d128f18127679508e910c2317ca2bbc9cd74a
A.Abramov <a.abramov@corp.nekki.ru>
2022-10-01 16:09:05 +0300

    Revert "test"
    This reverts commit 39b17d0963565a80b1075e3a8a40b6c39ed44d2f.

--- Drop/DropBase.cs
+++ Drop/DropBase.cs
@@ -16,7 +16,7 @@ namespace Drop
 
         private float _speed = 3f;
 
-        private bool _tossing = true; //test
+        private bool _tossing = true;
 
         private void Start()
         {
